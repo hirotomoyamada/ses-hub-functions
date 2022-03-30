4b28bfc51da8b80bdcb50f81578cfe433c5f6394
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-03-31 00:34:41 +0900

    collection から取得

--- src/ses_hub/user/request.ts
+++ src/ses_hub/user/request.ts
@@ -1,9 +1,10 @@
 import * as functions from "firebase-functions";
-import { converter, db, location, runtime } from "../../firebase";
+import { converter, db, location, runtime } from "firebase";
 import { userAuthenticated } from "./_userAuthenticated";
-import { send } from "../../sendgrid";
-import * as body from "../mail";
-import * as Firestore from "../../types/firestore";
+import { send } from "sendgrid";
+import * as body from "ses_hub/mail";
+import * as Firestore from "types/firestore";
+import { PartiallyPartial } from "types/utils";
 
 type Data = {
   uid: string;
@@ -15,26 +16,31 @@ export const addRequest = functions
   .runWith(runtime)
   .https.onCall(async (data: Data, context) => {
     await userAuthenticated({
-      context: context,
+      context,
       demo: true,
       index: "persons",
     });
 
-    await updatePerson(context, data);
+    const { user, selectUser } = await updateFirestore(context, data);
+
+    await sendMail(
+      user as Firestore.Company,
+      selectUser as Firestore.Person,
+      data.body
+    );
 
     return;
   });
 
 const sendMail = async (
-  context: functions.https.CallableContext,
   user: Firestore.Company,
   selectUser: Firestore.Person,
-  data: Data
+  text: string
 ): Promise<void> => {
   const url = {
-    user: `${functions.config().app.ses_hub.url}/persons/${data.uid}`,
+    user: `${functions.config().app.ses_hub.url}/persons/${selectUser.uid}`,
     selectUser: `${functions.config().app.freelance_direct.url}/user/${
-      context.auth?.uid
+      user.uid
     }`,
   };
 
@@ -45,7 +51,7 @@ const sendMail = async (
       subject: "【リクエスト】確認メール",
       text: body.request.user({
         user: user.profile,
-        body: data.body,
+        body: text,
         url: url.user,
       }),
     },
@@ -56,7 +62,7 @@ const sendMail = async (
       subject: `【リクエスト】${user.profile.name} ${user.profile.person}さんから、リクエストがありました`,
       text: body.request.selectUser({
         user: selectUser.profile,
-        body: data.body,
+        body: text,
         url: url.selectUser,
       }),
     },
@@ -66,17 +72,10 @@ const sendMail = async (
   await send(mail.selectUser);
 };
 
-const updateDoc = async ({
-  context,
-  doc,
-  data,
-  user,
-}: {
-  context: functions.https.CallableContext;
-  doc: FirebaseFirestore.DocumentSnapshot<Firestore.Person | Firestore.Company>;
-  data: Data;
-  user?: boolean;
-}): Promise<void> => {
+const updateFirestore = async (
+  context: functions.https.CallableContext,
+  data: Data
+) => {
   if (!context.auth) {
     throw new functions.https.HttpsError(
       "unauthenticated",
@@ -84,151 +83,73 @@ const updateDoc = async ({
       "auth"
     );
   }
+
+  const user: PartiallyPartial<Firestore.Company, "uid"> = {
+    uid: context.auth.uid,
+  };
+  const selectUser: PartiallyPartial<Firestore.Person, "uid"> = {
+    uid: data.uid,
+  };
 
   const timestamp = Date.now();
 
-  if (!user) {
-    const selectUser = doc.data() as Firestore.Person;
-    const hold = selectUser.requests.hold;
+  for await (const index of ["persons", "companys"]) {
+    const person = index === "persons";
 
-    await doc.ref
-      .set(
-        {
-          requests: Object.assign(selectUser.requests, {
-            hold: [context.auth.uid, ...(hold as string[])],
-          }),
-        },
-        { merge: true }
-      )
-      .then(async () => {
-        await updateUser(context, data, selectUser);
-      })
+    const collection = db
+      .collection(index)
+      .doc(person ? selectUser.uid : user.uid)
+      .withConverter(converter<Firestore.Company | Firestore.Person>());
+
+    const subCollection = collection
+      .collection(person ? "requests" : "entries")
+      .withConverter(converter<Firestore.User>());
+
+    const querySnapshot = await subCollection
+      .where("uid", "==", person ? user.uid : selectUser.uid)
+      .get()
       .catch(() => {
         throw new functions.https.HttpsError(
-          "data-loss",
-          "リクエストの追加に失敗しました",
+          "not-found",
+          "コレクションの取得に失敗しました",
           "firebase"
         );
       });
-  } else {
-    const user = doc.data() as Firestore.Company;
-    const entries = user.entries.persons;
 
-    await doc.ref
-      .set(
-        {
-          entries: Object.assign(user.entries, {
-            persons: entries
-              ? (entries as string[]).indexOf(data.uid) < 0 && [
-                  data.uid,
-                  ...entries,
-                ]
-              : [data.uid],
-          }),
-          updateAt: timestamp,
-        },
-        { merge: true }
-      )
-      .catch(() => {
-        throw new functions.https.HttpsError(
-          "data-loss",
-          "エントリーの追加に失敗しました",
-          "firebase"
-        );
-      });
-  }
-};
+    if (!querySnapshot.docs[0]) {
+      await subCollection
+        .add({
+          index: person ? "companys" : "persons",
+          uid: person ? user.uid : selectUser.uid,
+          status: "hold",
+          active: true,
+          createAt: timestamp,
+        })
+        .catch(() => {
+          throw new functions.https.HttpsError(
+            "data-loss",
+            "データの追加に失敗しました",
+            "firebase"
+          );
+        });
+    } else {
+      throw new functions.https.HttpsError(
+        "data-loss",
+        "すでにリクエスト済みのため、処理中止",
+        "firebase"
+      );
+    }
 
-const checkDuplicate = (
-  context: functions.https.CallableContext,
-  doc: FirebaseFirestore.DocumentSnapshot<Firestore.Person>
-): void => {
-  if (!context.auth) {
-    throw new functions.https.HttpsError(
-      "unauthenticated",
-      "認証されていないユーザーではログインできません",
-      "auth"
-    );
-  }
-
-  const enable = doc.data()?.requests?.enable;
-  const hold = doc.data()?.requests?.hold;
-  const disable = doc.data()?.requests?.disable;
-
-  if (
-    (enable as string[]).indexOf(context.auth.uid) >= 0 ||
-    (hold as string[]).indexOf(context.auth.uid) >= 0 ||
-    (disable as string[]).indexOf(context.auth.uid) >= 0
-  ) {
-    throw new functions.https.HttpsError(
-      "data-loss",
-      "すでにリクエスト済みのため、処理中止",
-      "firebase"
-    );
-  }
-
-  return;
-};
-
-const updateUser = async (
-  context: functions.https.CallableContext,
-  data: Data,
-  selectUser: Firestore.Person
-): Promise<void> => {
-  if (!context.auth) {
-    throw new functions.https.HttpsError(
-      "unauthenticated",
-      "認証されていないユーザーではログインできません",
-      "auth"
-    );
-  }
-
-  const doc = await db
-    .collection("companys")
-    .withConverter(converter<Firestore.Company>())
-    .doc(context.auth.uid)
-    .get()
-    .catch(() => {
+    const doc = await collection.get().catch(() => {
       throw new functions.https.HttpsError(
         "not-found",
-        "ユーザーの取得に失敗しました",
+        "データの取得に失敗しました",
         "firebase"
       );
     });
 
-  if (doc.exists) {
-    const user = doc.data();
-
-    await sendMail(context, user as Firestore.Company, selectUser, data);
-
-    await updateDoc({ context: context, doc: doc, data: data, user: true });
+    Object.assign(person ? selectUser : user, doc.data());
   }
 
-  return;
-};
-
-const updatePerson = async (
-  context: functions.https.CallableContext,
-  data: Data
-): Promise<void> => {
-  const doc = await db
-    .collection("persons")
-    .withConverter(converter<Firestore.Person>())
-    .doc(data.uid)
-    .get()
-    .catch(() => {
-      throw new functions.https.HttpsError(
-        "not-found",
-        "ユーザーの取得に失敗しました",
-        "firebase"
-      );
-    });
-
-  if (doc.exists) {
-    checkDuplicate(context, doc);
-
-    await updateDoc({ context: context, doc: doc, data: data });
-  }
-
-  return;
+  return { user, selectUser };
 };

38a2fc6b5316076518be9fd7c719f7d0560e35ce
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-04-10 21:39:24 +0900

    add index, uid, icon, nickName, num, at in dummy

--- src/_utils.ts
+++ src/_utils.ts
@@ -1,31 +1,115 @@
-import { company, lastName, firstName, email, urls } from "./_dummy";
+import {
+  company,
+  lastNickName,
+  firstNickName,
+  lastName,
+  firstName,
+  email,
+  urls,
+} from "./_dummy";
+import { randomBytes } from "crypto";
 
-export type Today = {
-  start: () => number;
-  end: () => number;
+export type Time = (t: "day" | "week" | "month") => {
+  start: number;
+  end: number;
 };
 
-export const today: Today = {
-  start: (): number => new Date().setHours(0, 0, 0, 0),
-  end: (): number => new Date().setHours(23, 59, 59, 999),
+export const time: Time = (t = "day") => {
+  const end = new Date().setHours(23, 59, 59, 999);
+
+  switch (t) {
+    case "month": {
+      const start = new Date(new Date().setDate(1)).setHours(0, 0, 0, 0);
+
+      return { start, end };
+    }
+    case "week": {
+      let timestamp = new Date();
+
+      while (true) {
+        if (timestamp.getDay() == 1) break;
+        timestamp = new Date(
+          timestamp.getFullYear(),
+          timestamp.getMonth(),
+          timestamp.getDate() - 1
+        );
+      }
+
+      const start = timestamp.getTime();
+
+      return { start, end };
+    }
+    default: {
+      const start = new Date().setHours(0, 0, 0, 0);
+
+      return { start, end };
+    }
+  }
 };
 
 export type Dummy = {
+  index: () => "companys" | "persons";
+  uid: () => string;
+  icon: (i: string) => string;
   name: () => string;
   person: () => string;
+  nickName: () => string;
   email: () => string;
   urls: (i: number) => string[];
+  num: (min: number, max?: number) => number;
+  at: (t?: "day" | "week" | "month") => number;
 };
 
 export const dummy: Dummy = {
-  name: (): string => company[Math.floor(Math.random() * company.length)],
-  person: (): string =>
+  index: (): "companys" | "persons" =>
+    ["companys", "persons"][Math.floor(Math.random() * 2)] as
+      | "companys"
+      | "persons",
+  uid: () => {
+    const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const N = 28;
+    return Array.from(randomBytes(N))
+      .map((n) => S[n % S.length])
+      .join("");
+  },
+  icon: (i) =>
+    i === "companys"
+      ? `icon${Math.floor(Math.random() * 17 + 1)}`
+      : `icon${Math.floor(Math.random() * (36 - 17 + 1) + 17 + 1)}`,
+  name: () => company[Math.floor(Math.random() * company.length)],
+  person: () =>
     `${lastName[Math.floor(Math.random() * lastName.length)]}${
       firstName[Math.floor(Math.random() * firstName.length)]
     }`,
-  email: (): string => email[Math.floor(Math.random() * email.length)],
-  urls: (i: number): string[] =>
+  nickName: () =>
+    lastNickName[Math.floor(Math.random() * lastNickName.length)] +
+    firstNickName[Math.floor(Math.random() * firstNickName.length)],
+  email: () => email[Math.floor(Math.random() * email.length)],
+  urls: (i) =>
     [...Array(Math.floor(Math.random() * (i ? i : 1) + 1))].map(
       () => [...urls].splice(Math.floor(Math.random() * [...urls].length), 1)[0]
     ),
+  num: (min, max) =>
+    !max
+      ? Math.floor(Math.random() * min + 1)
+      : Math.floor(Math.random() * (max - min + 1) + min + 1),
+  at: (t = "day") => {
+    const now = Date.now();
+    const day = 60 * 60 * 24 * 1000;
+
+    switch (t) {
+      case "month": {
+        const target = now - day * 31;
+        return Math.floor(Math.random() * (now - target + 1) + target + 1);
+      }
+      case "week": {
+        const target = now - day * 7;
+        return Math.floor(Math.random() * (now - target + 1) + target + 1);
+      }
+      default: {
+        const target = now - day * 1;
+        return Math.floor(Math.random() * (now - target + 1) + target + 1);
+      }
+    }
+  },
 };

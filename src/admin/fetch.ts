7e71e0818219f93248ab48272283283f1ecf21a3
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-05-12 14:53:36 +0900

    fetchPost コード 整形

--- src/admin/fetch.ts
+++ src/admin/fetch.ts
@@ -8,6 +8,8 @@ import * as Auth from "../types/auth";
 import * as Algolia from "../types/algolia";
 
 type Arg = {
+  post: { index: "matters" | "persons"; objectID: string };
+
   posts: {
     index: "matters" | "resources" | "companys" | "persons";
     target: string | undefined;
@@ -20,17 +22,27 @@ type Arg = {
   user: { index: "companys" | "persons"; uid: string };
 };
 
+type Post = Algolia.Matter | Algolia.Resource;
+
 type Posts =
   | Algolia.Matter
   | Algolia.Resource
   | Algolia.Company
   | Algolia.Person;
 
-type Results = Algolia.Matter | Algolia.Resource | Auth.Company | Auth.Person;
-
 type Collections = {
   [key: string]: string[] | { [key: string]: string[] };
 };
+
+export const fetchPost = functions
+  .region(location)
+  .runWith(runtime)
+  .https.onCall(async (arg: Arg["post"], context) => {
+    await userAuthenticated(context);
+    const { post } = await fetchAlgolia.post(arg);
+
+    return { index: arg.index, post };
+  });
 
 export const fetchPosts = functions
   .region(location)
@@ -43,11 +55,11 @@ export const fetchPosts = functions
 
       return { index: arg.index, posts: posts };
     } else {
-      const { posts, hit } = await fetchAlgolia(arg);
+      const { posts, hit } = await fetchAlgolia.posts(arg);
 
-      posts.length && (await fetchFirestore(arg, posts));
+      posts.length && (await fetchFirestore.posts(arg, posts));
 
-      return { index: arg.index, posts: posts, hit: hit };
+      return { index: arg.index, posts, hit };
     }
   });
 
@@ -57,7 +69,7 @@ export const fetchUser = functions
   .https.onCall(async (arg: Arg["user"], context) => {
     await userAuthenticated(context);
 
-    const user = await fetchFirestore(arg);
+    const user = await fetchFirestore.user(arg);
 
     if (!user) {
       throw new functions.https.HttpsError(
@@ -122,11 +134,13 @@ const fetchParent = async (uid?: string): Promise<Auth.Company | undefined> => {
   return parent;
 };
 
-const fetchFirestore = async (
-  arg: Arg["posts"] | Arg["user"],
-  posts?: Results[]
-): Promise<Auth.Company | Auth.Person | void> => {
-  if (posts?.length) {
+const fetchFirestore = {
+  posts: async (
+    arg: Arg["posts"],
+    posts?: (Algolia.Matter | Algolia.Resource | Auth.Company | Auth.Person)[]
+  ): Promise<void> => {
+    if (!posts?.length) return;
+
     for (let i = 0; i < posts.length; i++) {
       const index = arg.index !== "persons" ? "companys" : "persons";
       const post = posts[i];
@@ -155,9 +169,7 @@ const fetchFirestore = async (
 
       const data = doc.data();
 
-      if (!doc.exists || !data) {
-        return;
-      }
+      if (!doc.exists || !data) return;
 
       if (
         (arg.index === "matters" || arg.index === "resources") &&
@@ -191,7 +203,11 @@ const fetchFirestore = async (
         Object.assign(post, collections);
       }
     }
-  } else if ("uid" in arg) {
+  },
+
+  user: async (
+    arg: Arg["user"]
+  ): Promise<Auth.Company | Auth.Person | void> => {
     const doc = await db
       .collection(arg.index)
       .withConverter(converter<Firestore.Company | Firestore.Person>())
@@ -249,9 +265,7 @@ const fetchFirestore = async (
     }
 
     return;
-  }
-
-  return;
+  },
 };
 
 const fetchCollections = async ({
@@ -357,53 +371,81 @@ const fetchCollections = async ({
   return collections;
 };
 
-const fetchAlgolia = async (
-  arg: Arg["posts"]
-): Promise<{
-  posts: Results[];
-  hit: Algolia.Hit;
-}> => {
-  const index = algolia.initIndex(
-    !arg.target ||
-      ((arg.index === "matters" || arg.index === "resources") &&
-        arg.target === "createAt") ||
-      ((arg.index === "companys" || arg.index === "persons") &&
-        arg.target === "lastLogin")
-      ? arg.index
-      : `${arg.index}_${arg.target}_${arg.type}`
-  );
+const fetchAlgolia = {
+  post: async (
+    arg: Arg["post"]
+  ): Promise<{ post: Algolia.Matter | Algolia.Resource }> => {
+    const index = algolia.initIndex(arg.index);
 
-  const hit: Algolia.Hit = {
-    currentPage: arg.page ? arg.page : 0,
-  };
-
-  const result = await index
-    .search<Posts>(arg.value ? arg.value : "", {
-      page: hit.currentPage,
-      filters: arg.filter === "all" ? "" : arg.filter,
-    })
-    .catch(() => {
+    const hit = await index.getObject<Post>(arg.objectID).catch(() => {
       throw new functions.https.HttpsError(
         "not-found",
         "投稿の取得に失敗しました",
-        "algolia"
+        "notFound"
       );
     });
 
-  hit.posts = result?.nbHits;
-  hit.pages = result?.nbPages;
-
-  const posts = result?.hits
-    ?.map((hit) =>
+    const post =
       arg.index === "matters"
         ? fetch.matter(<Algolia.Matter>hit)
-        : arg.index === "resources"
-        ? fetch.resource(<Algolia.Resource>hit)
-        : arg.index === "companys"
-        ? fetch.company.item(<Algolia.Company>hit)
-        : arg.index === "persons" && fetch.person.item(<Algolia.Person>hit)
-    )
-    ?.filter((post) => post) as Results[];
+        : fetch.resource(<Algolia.Resource>hit);
 
-  return { posts, hit };
+    return { post };
+  },
+
+  posts: async (
+    arg: Arg["posts"]
+  ): Promise<{
+    posts: (Algolia.Matter | Algolia.Resource | Auth.Company | Auth.Person)[];
+    hit: Algolia.Hit;
+  }> => {
+    const index = algolia.initIndex(
+      !arg.target ||
+        ((arg.index === "matters" || arg.index === "resources") &&
+          arg.target === "createAt") ||
+        ((arg.index === "companys" || arg.index === "persons") &&
+          arg.target === "lastLogin")
+        ? arg.index
+        : `${arg.index}_${arg.target}_${arg.type}`
+    );
+
+    const hit: Algolia.Hit = {
+      currentPage: arg.page ? arg.page : 0,
+    };
+
+    const result = await index
+      .search<Posts>(arg.value ? arg.value : "", {
+        page: hit.currentPage,
+        filters: arg.filter === "all" ? "" : arg.filter,
+      })
+      .catch(() => {
+        throw new functions.https.HttpsError(
+          "not-found",
+          "投稿の取得に失敗しました",
+          "algolia"
+        );
+      });
+
+    hit.posts = result?.nbHits;
+    hit.pages = result?.nbPages;
+
+    const posts = result?.hits
+      ?.map((hit) =>
+        arg.index === "matters"
+          ? fetch.matter(<Algolia.Matter>hit)
+          : arg.index === "resources"
+          ? fetch.resource(<Algolia.Resource>hit)
+          : arg.index === "companys"
+          ? fetch.company.item(<Algolia.Company>hit)
+          : arg.index === "persons" && fetch.person.item(<Algolia.Person>hit)
+      )
+      ?.filter((post) => post) as (
+      | Algolia.Matter
+      | Algolia.Resource
+      | Auth.Company
+      | Auth.Person
+    )[];
+
+    return { posts, hit };
+  },
 };

1f8d0f751c7e6706b161da068410f2fe8a3bf41a
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-05-25 22:58:16 +0900

    アクティビティ ログからエンジニアを除外

--- src/ses_hub/post/activity.ts
+++ src/ses_hub/post/activity.ts
@@ -277,7 +277,7 @@ const fetchCollectionGroup = async (
         .withConverter(converter<Firestore.Post>())
         .where("index", "==", data.index)
         .where("objectID", "==", data.post.objectID)
-        .where("payment", "!=", null)
+        .where("payment", "in", ["canceled", "active", "trialing"])
         .orderBy("createAt", "desc")
         .get()
         .catch(() => {});

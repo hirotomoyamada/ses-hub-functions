5496bf458b48b935617193df9f7a91e781d4ab61
hirotomo.yamada <combined_basilisk@pfg.app>
2022-10-14 16:50:25 +0900

    コレクション内ドキュメントがヒットしない場合エラーを出す

--- src/admin/post.ts
+++ src/admin/post.ts
@@ -1,13 +1,13 @@
-import * as functions from "firebase-functions";
-import { converter, db, location, runtime } from "../_firebase";
-import { algolia } from "../_algolia";
-import { userAuthenticated } from "./_userAuthenticated";
-import * as Algolia from "../types/algolia";
-import * as format from "./_format";
-import * as Firestore from "../types/firestore";
+import * as functions from 'firebase-functions';
+import { converter, db, location, runtime } from '../_firebase';
+import { algolia } from '../_algolia';
+import { userAuthenticated } from './_userAuthenticated';
+import * as Algolia from '../types/algolia';
+import * as format from './_format';
+import * as Firestore from '../types/firestore';
 
 type Data = {
-  index: "matters" | "resources";
+  index: 'matters' | 'resources';
   post: Algolia.Matter | Algolia.Resource;
 };
 
@@ -39,16 +39,16 @@ export const deletePost = functions
 const editAlgolia = async (data: Data): Promise<void> => {
   const index = algolia.initIndex(data.index);
   const post =
-    data.index === "matters"
+    data.index === 'matters'
       ? format.matter(<Algolia.Matter>data.post)
-      : data.index === "resources" &&
+      : data.index === 'resources' &&
         format.resource(<Algolia.Resource>data.post);
 
   if (!post) {
     throw new functions.https.HttpsError(
-      "data-loss",
-      "投稿の編集に失敗しました",
-      "algolia"
+      'data-loss',
+      '投稿の編集に失敗しました',
+      'algolia',
     );
   }
 
@@ -58,9 +58,9 @@ const editAlgolia = async (data: Data): Promise<void> => {
     })
     .catch(() => {
       throw new functions.https.HttpsError(
-        "data-loss",
-        "投稿の編集に失敗しました",
-        "algolia"
+        'data-loss',
+        '投稿の編集に失敗しました',
+        'algolia',
       );
     });
 };
@@ -74,23 +74,30 @@ const updateFirestore = async ({
 }): Promise<void> => {
   const timestamp = Date.now();
 
-  const ref = db.collection("companys").doc(data.post.uid);
+  const ref = db.collection('companys').doc(data.post.uid);
 
   const collection = ref
-    .collection("posts")
+    .collection('posts')
     .withConverter(converter<Firestore.Post>());
 
   const querySnapshot = await collection
-    .where("index", "==", data.index)
-    .where("objectID", "==", data.post.objectID)
+    .where('index', '==', data.index)
+    .where('objectID', '==', data.post.objectID)
     .get()
     .catch(() => {
       throw new functions.https.HttpsError(
-        "not-found",
-        "コレクションの取得に失敗しました",
-        "firebase"
+        'not-found',
+        'コレクションの取得に失敗しました',
+        'firebase',
       );
     });
+
+  if (!querySnapshot.docs.length)
+    throw new functions.https.HttpsError(
+      'not-found',
+      '投稿の取得に失敗しました',
+      'firebase',
+    );
 
   const doc = querySnapshot.docs[0];
 
@@ -98,16 +105,16 @@ const updateFirestore = async ({
     .set(
       edit
         ? { display: data.post.display, updateAt: timestamp }
-        : { active: false, display: "private", deleteAt: timestamp },
+        : { active: false, display: 'private', deleteAt: timestamp },
       {
         merge: true,
-      }
+      },
     )
     .catch(() => {
       throw new functions.https.HttpsError(
-        "data-loss",
-        "データの更新に失敗しました",
-        "firebase"
+        'data-loss',
+        'データの更新に失敗しました',
+        'firebase',
       );
     });
 
@@ -115,16 +122,16 @@ const updateFirestore = async ({
 };
 
 const updateCollectionGroup = async (data: Data) => {
-  const collections = ["likes", "outputs", "entries", "histories"];
+  const collections = ['likes', 'outputs', 'entries', 'histories'];
 
   await Promise.allSettled(
     collections.map(async (collection) => {
       const querySnapshot = await db
         .collectionGroup(collection)
         .withConverter(converter<Firestore.Post>())
-        .where("index", "==", data.index)
-        .where("objectID", "==", data.post.objectID)
-        .orderBy("createAt", "desc")
+        .where('index', '==', data.index)
+        .where('objectID', '==', data.post.objectID)
+        .orderBy('createAt', 'desc')
         .get()
         .catch(() => {});
 
@@ -141,7 +148,7 @@ const updateCollectionGroup = async (data: Data) => {
             .catch(() => {});
         }
       });
-    })
+    }),
   );
 };
 
@@ -149,9 +156,9 @@ const deleteAlgolia = async (data: Data): Promise<void> => {
   const index = algolia.initIndex(data.index);
   await index.deleteObject(data.post.objectID).catch(() => {
     throw new functions.https.HttpsError(
-      "data-loss",
-      "投稿の削除に失敗しました",
-      "algolia"
+      'data-loss',
+      '投稿の削除に失敗しました',
+      'algolia',
     );
   });
 

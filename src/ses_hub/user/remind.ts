ca8394965b31102a0ae34f7179acb74d4d66a57b
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-07-29 08:40:57 +0900

    fix: disabled remind send, enable

--- src/ses_hub/user/remind.ts
+++ src/ses_hub/user/remind.ts
@@ -1,111 +1,117 @@
 import * as functions from 'firebase-functions';
-import { converter, db, location, runtime, timeZone } from '../../_firebase';
+import {
+  converter,
+  db,
+  location,
+  runtime,
+  // timeZone
+} from '../../_firebase';
 import { userAuthenticated } from './_userAuthenticated';
 import * as Firestore from '../../types/firestore';
 import { log } from '../../_utils';
 import { send } from '../../_sendgrid';
 import * as body from '../mail';
 
-export const sendRemind = functions
-  .region(location)
-  .runWith(runtime)
-  .pubsub.schedule('0 13 */2 * *')
-  .timeZone(timeZone)
-  .onRun(async () => {
-    const { docs } = await db
-      .collection('companys')
-      .withConverter(converter<Firestore.Company>())
-      .get();
+// export const sendRemind = functions
+//   .region(location)
+//   .runWith(runtime)
+//   .pubsub.schedule('0 13 */2 * *')
+//   .timeZone(timeZone)
+//   .onRun(async () => {
+//     const { docs } = await db
+//       .collection('companys')
+//       .withConverter(converter<Firestore.Company>())
+//       .get();
 
-    const url: string = functions.config().app.ses_hub.url;
+//     const url: string = functions.config().app.ses_hub.url;
 
-    const timestamp = Date.now();
-    const threeDaysAgo = timestamp - 3 * 24 * 60 * 60 * 1000;
+//     const timestamp = Date.now();
+//     const threeDaysAgo = timestamp - 3 * 24 * 60 * 60 * 1000;
 
-    await Promise.allSettled(
-      docs.map(async (doc) => {
-        const data = doc.data();
+//     await Promise.allSettled(
+//       docs.map(async (doc) => {
+//         const data = doc.data();
 
-        const { createAt, profile } = data;
+//         const { createAt, profile } = data;
 
-        if (threeDaysAgo < createAt) return;
+//         if (threeDaysAgo < createAt) return;
 
-        const { docs } =
-          (await doc.ref
-            .collection('posts')
-            .withConverter(converter<Firestore.Post>())
-            .orderBy('createAt', 'desc')
-            .limit(1)
-            .get()
-            .catch(() => {})) ?? {};
+//         const { docs } =
+//           (await doc.ref
+//             .collection('posts')
+//             .withConverter(converter<Firestore.Post>())
+//             .orderBy('createAt', 'desc')
+//             .limit(1)
+//             .get()
+//             .catch(() => {})) ?? {};
 
-        const latestPostCreateAt = (docs ?? [])[0]?.data()?.createAt ?? 0;
+//         const latestPostCreateAt = (docs ?? [])[0]?.data()?.createAt ?? 0;
 
-        if (threeDaysAgo < latestPostCreateAt) return;
+//         if (threeDaysAgo < latestPostCreateAt) return;
 
-        const userMail = {
-          to: profile.email,
-          from: `SES_HUB <${functions.config().admin.ses_hub}>`,
-          subject: 'SES_HUB 案件/人材情報ご登録のススメ',
-          text: body.remind.user(url),
-        };
+//         const userMail = {
+//           to: profile.email,
+//           from: `SES_HUB <${functions.config().admin.ses_hub}>`,
+//           subject: 'SES_HUB 案件/人材情報ご登録のススメ',
+//           text: body.remind.user(url),
+//         };
 
-        await send(userMail);
+//         await send(userMail);
 
-        await log({
-          auth: { collection: 'companys', doc: doc.id },
-          run: 'sendRemind',
-          code: 200,
-        });
-      }),
-    );
-  });
+//         await log({
+//           auth: { collection: 'companys', doc: doc.id },
+//           run: 'sendRemind',
+//           code: 200,
+//         });
+//       }),
+//     );
+//   });
 
-export const enableRemind = functions
-  .region(location)
-  .runWith(runtime)
-  .pubsub.schedule('0 13 * * *')
-  .timeZone(timeZone)
-  .onRun(async () => {
-    const { docs } = await db
-      .collection('companys')
-      .withConverter(converter<Firestore.Company>())
-      .get();
+// export const enableRemind = functions
+//   .region(location)
+//   .runWith(runtime)
+//   .pubsub.schedule('0 13 * * *')
+//   .timeZone(timeZone)
+//   .onRun(async () => {
+//     const { docs } = await db
+//       .collection('companys')
+//       .withConverter(converter<Firestore.Company>())
+//       .get();
 
-    const timestamp = Date.now();
-    const threeDaysAgo = timestamp - 3 * 24 * 60 * 60 * 1000;
+//     const timestamp = Date.now();
+//     const threeDaysAgo = timestamp - 3 * 24 * 60 * 60 * 1000;
 
-    await Promise.allSettled(
-      docs.map(async (doc) => {
-        const data = doc.data();
+//     await Promise.allSettled(
+//       docs.map(async (doc) => {
+//         const data = doc.data();
 
-        const { createAt } = data;
+//         const { createAt } = data;
 
-        if (threeDaysAgo < createAt) return;
+//         if (threeDaysAgo < createAt) return;
 
-        const { docs } =
-          (await doc.ref
-            .collection('posts')
-            .withConverter(converter<Firestore.Post>())
-            .orderBy('createAt', 'desc')
-            .limit(1)
-            .get()
-            .catch(() => {})) ?? {};
+//         const { docs } =
+//           (await doc.ref
+//             .collection('posts')
+//             .withConverter(converter<Firestore.Post>())
+//             .orderBy('createAt', 'desc')
+//             .limit(1)
+//             .get()
+//             .catch(() => {})) ?? {};
 
-        const latestPostCreateAt = (docs ?? [])[0]?.data()?.createAt ?? 0;
+//         const latestPostCreateAt = (docs ?? [])[0]?.data()?.createAt ?? 0;
 
-        if (threeDaysAgo < latestPostCreateAt) return;
+//         if (threeDaysAgo < latestPostCreateAt) return;
 
-        await updateFiresotre(doc.id, 'enable');
+//         await updateFiresotre(doc.id, 'enable');
 
-        await log({
-          auth: { collection: 'companys', doc: doc.id },
-          run: 'enableRemind',
-          code: 200,
-        });
-      }),
-    );
-  });
+//         await log({
+//           auth: { collection: 'companys', doc: doc.id },
+//           run: 'enableRemind',
+//           code: 200,
+//         });
+//       }),
+//     );
+//   });
 
 export const disableRemind = functions
   .region(location)
